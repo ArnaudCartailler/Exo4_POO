b1f4a3764c4bf41ff77f310feec9a02b0815c17d
arnaudcartailler <arnaud.cartailler@gmail.com>
2018-11-05 15:19:05 +0100

    add ex 4

---
+++ js/main.js
@@ -0,0 +1,40 @@
+function ajaxGet(url, callback) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            // Appelle la fonction callback en lui passant la réponse de la requête
+            callback(req.responseText);
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    req.send(null);
+}
+
+ajaxGet("http://localhost/exo4_poo/main.json", function (reponse) {
+
+    var data = JSON.parse(reponse);
+
+    data.person.forEach(user => {
+        const addline = document.getElementById('addLine');
+
+        const tr = document.createElement('tr');
+
+        const td1 = document.createElement('td');
+        const td2 = document.createElement('td');
+        const td3 = document.createElement('td');
+
+        td1.innerHTML = user.firstname
+        td2.innerHTML = user.lastname
+        td3.innerHTML = user.age
+
+        addline.appendChild(tr);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+    });
+});
